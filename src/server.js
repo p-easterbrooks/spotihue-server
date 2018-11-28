c11c5e8823cbec25c38c064680cbc835f4d63b6a
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2018-11-28 14:22:22 -0500

    successfully pull in images to node server

--- src/server.js
+++ src/server.js
@@ -1,69 +1,76 @@
 // content of index.js
+//imports
 const http = require('http')
 const port = 3000
 const request = require('request')
 const Poller = require('./Poller')
 const ColorThief = require('@mariotacke/color-thief')
-var Jimp = require('jimp')
+var Canvas = require('canvas')
+var fs = require('fs')
+var Image = Canvas.Image
+
+//global var
 var imgUrl
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+    console.log(request.url)
+    response.end('Hello Node.js Server!')
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad ${port} happened', err)
-  }
-  console.log(`server is listening on ${port}`)
+    if (err) {
+        return console.log('something bad ${port} happened', err)
+    }
+    console.log(`server is listening on ${port}`)
 
-  //new poller with 2 second timeout
-  let poller = new Poller(2000)
-  
-  // Wait till the timeout sent our event to the EventEmitter
-  poller.onPoll(() => {
-    console.log('polling');
+    //new poller with 2 second timeout
+    let poller = new Poller(2000)
 
-    const options = {
-      url: 'https://api.spotify.com/v1/me/player/currently-playing',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer BQDFlRTc56s68lF7m5Zo9VRAldyMdXu55wdNNU5IxfjYR8rhc9ZUbLniT-BGEjOkfnTlh3WLV5WpZJ1SphnQvo9QP3slwMX9WmFv42MRiTpEhtDoTLqJFp107kE9D6it1EmBIoMQ4zS-NypJhErrCAMuIGM'
-      }
-    };
-    request(options, doAction) 
+    // Wait till the timeout sent our event to the EventEmitter
+    poller.onPoll(() => {
+        console.log('polling');
 
-    poller.poll(); // Go for the next poll
-  });
+        const options = {
+            url: 'https://api.spotify.com/v1/me/player/currently-playing',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer BQBSaz5HT4kBLXpzlKeLQ9_BT9izdwl21LgnRLzVWgHyApsdi4Qr7nxReZLxBP0flXq1h8t2bLiMxGV7gJek5sJI18P9AaW6ULjpL8VMuaSQrAuwhu15wLRUdXXnJyItv_mtnsdbxPWSRlZt-4BhyTb_LfM'
+            }
+        };
+        request(options, doAction)
 
-  // Initial start
-  poller.poll();
+        poller.poll(); // Go for the next poll
+    });
+
+    // Initial start
+    poller.poll();
 })
 
-function doAction (error, response, body) {
-  const data = JSON.parse(body)
-  if (data.item.album.images[1].url !== imgUrl) {
-    console.log('NEW IMAGE')
-    imgUrl = data.item.album.images[1].url
-    console.log(`imgUrl: ${imgUrl}`)
+function doAction(error, response, body) {
+    const data = JSON.parse(body)
+    if (data.item.album.images[1].url !== imgUrl) {
+        console.log('NEW IMAGE')
+        imgUrl = data.item.album.images[1].url
+        console.log(`imgUrl: ${imgUrl}`)
+        //create canvas and image
+        var img = new Image()
+        img.src = imgUrl
 
-    //acquire image
-    var thief = new ColorThief()
-    Jimp.read(imgUrl)
-      .then(image => {
-        processColors(thief.getPalette(image))
-      })
-      .catch(err => {
-        console.log(`Error getting image from Spotify: ${err}`)
-      })
-  }
+        img.onload = function () {
+            var canvas = new Canvas.Canvas(300, 300)
+            var ctx = canvas.getContext('2d')
+            ctx.drawImage(img, 0, 0, 300, 300)
+            var thief = new ColorThief()
+            canvas.createJPEGStream().pipe(fs.createWriteStream('coverArt.jpeg'))
+            //callback(thief.getPalette(img, 5, 5))
+        }
+    }
 }
 
-function getCIEColor (color) {
+function getCIEColor(color) {
     var r = color[0]
     var g = color[1]
     var b = color[2]
@@ -72,28 +79,29 @@ function getCIEColor (color) {
 }
 
 function processColors(palette) {
-  var primaryColor = palette[0]
-  var secondaryColor = palette[1]
+    var primaryColor = palette[0]
+    var secondaryColor = palette[1]
 
-  var lampCIEColor = getCIEColor(primaryColor)
+    console.log(palette[0])
+    var lampCIEColor = getCIEColor(primaryColor)
 
-  //set color on ambiance light
-  //setLamp(lampCIEColor[0], lampCIEColor[1], 3)
+    //set color on ambiance light
+    //setLamp(lampCIEColor[0], lampCIEColor[1], 3)
 }
 
-function setLamp (x, y, lightNumber) {
-  var myX = Number(x)
-  var myY = Number(y)
-  var hubIP = '192.168.1.219'
-  var username = '974DELC9EApDxKHu3W5P2fjMCE7YWbrM2LmVRoJv'
-  var URL = 'http://' + hubIP + '/api/' + username + '/lights/' + lightNumber + '/state'
-  var dataObject = {'on': true, 'sat': 254, 'bri': 254, 'xy': [ myX, myY]}
+function setLamp(x, y, lightNumber) {
+    var myX = Number(x)
+    var myY = Number(y)
+    var hubIP = '192.168.1.219'
+    var username = '974DELC9EApDxKHu3W5P2fjMCE7YWbrM2LmVRoJv'
+    var URL = 'http://' + hubIP + '/api/' + username + '/lights/' + lightNumber + '/state'
+    var dataObject = { 'on': true, 'sat': 254, 'bri': 254, 'xy': [myX, myY] }
 
-  const options = {
-    url: URL,
-    json: true,
-    body: JSON.stringify(dataObject)
-  }
+    const options = {
+        url: URL,
+        json: true,
+        body: JSON.stringify(dataObject)
+    }
 
-  request.put(options);
+    request.put(options);
 }
