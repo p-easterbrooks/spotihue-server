65fdbbeb85b7f042c85717fb6cf59694cbbea52c
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2018-11-28 15:05:28 -0500

    successful node service!

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const http = require('http')
 const port = 3000
 const request = require('request')
 const Poller = require('./Poller')
-const ColorThief = require('@mariotacke/color-thief')
+const ColorThief = require('./color-thief')
+const ColorConverter = require('./color-converter')
 var Canvas = require('canvas')
 var fs = require('fs')
 var Image = Canvas.Image
@@ -37,7 +38,7 @@ server.listen(port, (err) => {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer BQBSaz5HT4kBLXpzlKeLQ9_BT9izdwl21LgnRLzVWgHyApsdi4Qr7nxReZLxBP0flXq1h8t2bLiMxGV7gJek5sJI18P9AaW6ULjpL8VMuaSQrAuwhu15wLRUdXXnJyItv_mtnsdbxPWSRlZt-4BhyTb_LfM'
+                'Authorization': 'Bearer BQB2neq-3i2JnPsJ8razKyPgfG47h-m9GEkt1v4WQA0WDmoCYtRKbYr-xD9k1FxNDusIEQxn0QnQIaq-sQs5ExldOmc8Fp93cVRYqdwhelY71O0BFoPw0MevdW8Rk66VNJrgCCJszkEyCYGod5QrV4LRNIU'
             }
         };
         request(options, doAction)
@@ -55,18 +56,7 @@ function doAction(error, response, body) {
         console.log('NEW IMAGE')
         imgUrl = data.item.album.images[1].url
         console.log(`imgUrl: ${imgUrl}`)
-        //create canvas and image
-        var img = new Image()
-        img.src = imgUrl
-
-        img.onload = function () {
-            var canvas = new Canvas.Canvas(300, 300)
-            var ctx = canvas.getContext('2d')
-            ctx.drawImage(img, 0, 0, 300, 300)
-            var thief = new ColorThief()
-            canvas.createJPEGStream().pipe(fs.createWriteStream('coverArt.jpeg'))
-            //callback(thief.getPalette(img, 5, 5))
-        }
+        getPaletteFromURL(imgUrl, processColors)
     }
 }
 
@@ -78,12 +68,28 @@ function getCIEColor(color) {
     return rgb_to_cie(r, g, b)
 }
 
+function getPaletteFromURL(imageUrl, callback) {
+        //create canvas and image
+        var img = new Image()
+        img.src = imageUrl
+        img.crossOrigin = 'Anonymous'
+
+        img.onload = function () {
+            /*var canvas = new Canvas.Canvas(300, 300)
+            var ctx = canvas.getContext('2d')
+            ctx.drawImage(img, 0, 0, 300, 300)
+            var thief = new ColorThief()
+            canvas.createJPEGStream().pipe(fs.createWriteStream('coverArt.jpeg'))*/
+            callback(new ColorThief().getPalette(img, 5, 5))
+        }
+}
+
 function processColors(palette) {
     var primaryColor = palette[0]
     var secondaryColor = palette[1]
 
     console.log(palette[0])
-    var lampCIEColor = getCIEColor(primaryColor)
+    var lampCIEColor = ColorConverter.rgb_to_cie(primaryColor[0], primaryColor[1], primaryColor[2])
 
     //set color on ambiance light
     //setLamp(lampCIEColor[0], lampCIEColor[1], 3)
@@ -94,7 +100,7 @@ function setLamp(x, y, lightNumber) {
     var myY = Number(y)
     var hubIP = '192.168.1.219'
     var username = '974DELC9EApDxKHu3W5P2fjMCE7YWbrM2LmVRoJv'
-    var URL = 'http://' + hubIP + '/api/' + username + '/lights/' + lightNumber + '/state'
+    var URL = `http://${hubIP}/api/${username}/lights/${lightNumber}/state`
     var dataObject = { 'on': true, 'sat': 254, 'bri': 254, 'xy': [myX, myY] }
 
     const options = {
