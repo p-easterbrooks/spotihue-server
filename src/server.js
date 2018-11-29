ca88bb54d90c30f4bfa06c332b173068cdf11798
p-easterbrooks <peasterbrooks@perfectsensedigital.com>
2018-11-28 21:25:45 -0500

    lint

--- src/server.js
+++ src/server.js
@@ -1,33 +1,33 @@
 //imports
-const http = require('http')
-const port = 3000
-const request = require('request')
-const Poller = require('./Poller')
-const ColorThief = require('./color-thief')
-const ColorConverter = require('./color-converter')
+const http = require('http');
+const port = 3000;
+const request = require('request');
+const Poller = require('./Poller');
+const ColorThief = require('./color-thief');
+const ColorConverter = require('./color-converter');
 const chalk = require('chalk');
-var Image = require('canvas').Image
+var Image = require('canvas').Image;
 
 //global var
-var imgUrl
+var imgUrl;
 
 //TODO handle client requests
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end()
+    console.log(request.url);
+    response.end();
 }
 
 //server
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad ${port} happened', err)
+        return console.log('something bad ${port} happened', err);
     }
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`);
 
     //new poller with 2 second timeout
-    let poller = new Poller(2000)
+    let poller = new Poller(2000);
 
     // Wait till the timeout sent our event to the EventEmitter
     poller.onPoll(() => {
@@ -41,7 +41,7 @@ server.listen(port, (err) => {
                 'Authorization': 'Bearer BQAOrniWskRJp2tRsE3JUjhpn1nP5loQKko3BLzhIkxYehq_NZaED_nG1TINryjsGwwl0RdGmRZ06UrzUewI7gK3eVaR_W7IRUAWeHDT7OAwWre5QnYTvtF3iA9llbsgUpFYLKFrxpaQF9yVdo4M_XAfgQE'
             }
         };
-        request(options, doAction) 
+        request(options, doAction);
 
         poller.poll(); // Go for the next poll
     });
@@ -51,45 +51,45 @@ server.listen(port, (err) => {
 })
 
 function doAction(error, response, body) {
-    const data = JSON.parse(body)
+    const data = JSON.parse(body);
     if (data.item.album.images[1].url !== imgUrl) {
-        console.log('NEW IMAGE')
-        imgUrl = data.item.album.images[1].url
-        console.log(`imgUrl: ${imgUrl}`)
-        getPaletteFromURL(imgUrl, processColors)
+        console.log('NEW IMAGE');
+        imgUrl = data.item.album.images[1].url;
+        console.log(`imgUrl: ${imgUrl}`);
+        getPaletteFromURL(imgUrl, processColors);
     }
 }
 
 function getPaletteFromURL(imageUrl, callback) {
         //create canvas and image
-        var img = new Image()
-        img.src = imageUrl
-        img.crossOrigin = 'Anonymous'
+        var img = new Image();
+        img.src = imageUrl;
+        img.crossOrigin = 'Anonymous';
 
-        img.onload = () => callback(new ColorThief().getPalette(img, 5, 5))
+        img.onload = () => callback(new ColorThief().getPalette(img, 5, 5));
 }
 
 function processColors(palette) {
-    var primaryColor = palette[0]
-    var secondaryColor = palette[1]
-    var tertiaryColor = palette[2]
+    var primaryColor = palette[0];
+    var secondaryColor = palette[1];
+    var tertiaryColor = palette[2];
 
-    console.log(chalk.bold.rgb(primaryColor[0], primaryColor[1], primaryColor[2])('PRIMARY COLOR'))
-    console.log(chalk.bold.rgb(secondaryColor[0], secondaryColor[1], secondaryColor[2])('SECONDARY COLOR'))
-    console.log(chalk.bold.rgb(tertiaryColor[0], tertiaryColor[1], tertiaryColor[2])('TERTIARY COLOR'))
+    console.log(chalk.bold.rgb(primaryColor[0], primaryColor[1], primaryColor[2])('PRIMARY COLOR'));
+    console.log(chalk.bold.rgb(secondaryColor[0], secondaryColor[1], secondaryColor[2])('SECONDARY COLOR'));
+    console.log(chalk.bold.rgb(tertiaryColor[0], tertiaryColor[1], tertiaryColor[2])('TERTIARY COLOR'));
 
-    var lampCIEColor = ColorConverter.rgb_to_cie(primaryColor[0], primaryColor[1], primaryColor[2])
+    var lampCIEColor = ColorConverter.rgb_to_cie(primaryColor[0], primaryColor[1], primaryColor[2]);
 
     //set color on ambiance light
-    setLamp(lampCIEColor[0], lampCIEColor[1], 3)
+    setLamp(lampCIEColor[0], lampCIEColor[1], 3);
 }
 
 function setLamp(x, y, lightNumber) {
-    var myX = Number(x)
-    var myY = Number(y)
-    var hubIP = '192.168.1.219'
-    var username = '974DELC9EApDxKHu3W5P2fjMCE7YWbrM2LmVRoJv'
-    var URL = `http://${hubIP}/api/${username}/lights/${lightNumber}/state`
+    var myX = Number(x);
+    var myY = Number(y);
+    var hubIP = '192.168.1.219';
+    var username = '974DELC9EApDxKHu3W5P2fjMCE7YWbrM2LmVRoJv';
+    var URL = `http://${hubIP}/api/${username}/lights/${lightNumber}/state`;
 
     const options = {
         json: true,
